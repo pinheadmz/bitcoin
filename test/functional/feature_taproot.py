a51ee7c8187414ac6426f25d48de220abfb5d12f
Matthew Zipkin <pinheadmz@gmail.com>
2022-05-18 14:01:04 -0400

    add leaf script to test vectors

--- test/functional/feature_taproot.py
+++ test/functional/feature_taproot.py
@@ -399,6 +399,9 @@ def spend(tx, idx, utxos, **kwargs):
     """
 
     ctx = {**DEFAULT_CONTEXT, "tx":tx, "idx":idx, "utxos":utxos, **kwargs}
+    script = None
+    if ctx["leaf"] is not None:
+        script = get(ctx, "tapleaf").script.hex()
 
     def to_script(elem):
         """If fed a CScript, return it; if fed bytes, return a CScript that pushes it."""
@@ -410,7 +413,7 @@ def spend(tx, idx, utxos, **kwargs):
     scriptsig_list = flatten(get(ctx, "scriptsig"))
     scriptsig = CScript(b"".join(bytes(to_script(elem)) for elem in scriptsig_list))
     witness_stack = flatten(get(ctx, "witness"))
-    return (scriptsig, witness_stack, get(ctx, "annex"), get(ctx, "sighash"))
+    return (scriptsig, witness_stack, get(ctx, "annex"), get(ctx, "sighash"), script)
 
 
 # === Spender objects ===
@@ -1415,18 +1418,18 @@ class TaprootTest(BitcoinTestFramework):
                 input = input_data[i]
                 obj = {}
                 if input[0] != None:
-                    obj["fail"] =    {"scriptSig": input[0][0].hex(), "witness": [x.hex() for x in input[0][1]], "annex": input[0][2].hex() if input[0][2] is not None else None, "sighash": input[0][3].hex()}
+                    obj["fail"] =    {"scriptSig": input[0][0].hex(), "witness": [x.hex() for x in input[0][1]], "annex": input[0][2].hex() if input[0][2] is not None else None, "sighash": input[0][3].hex(), "script": input[0][4]}
                 else:
                     obj["fail"] = None
                 if input[1] != None:    
-                    obj["success"] = {"scriptSig": input[1][0].hex(), "witness": [x.hex() for x in input[1][1]], "annex": input[1][2].hex() if input[1][2] is not None else None, "sighash": input[1][3].hex()}
+                    obj["success"] = {"scriptSig": input[1][0].hex(), "witness": [x.hex() for x in input[1][1]], "annex": input[1][2].hex() if input[1][2] is not None else None, "sighash": input[1][3].hex(), "script": input[1][4]}
                 else:
                     obj["success"] = None
                 obj["comment"] = input[2]
                 obj["standard"] = input_utxos[i].spender.is_standard
                 jsonTX["inputs"].append(obj)
-            
             jsonTX["prevouts"] = [x.output.serialize().hex() for x in input_utxos]
+
             jsonAllTXs.append(jsonTX)
 
             # Sign each input incorrectly once on each complete signing pass, except the very last.
