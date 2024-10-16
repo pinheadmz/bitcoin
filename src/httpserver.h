eeacd007ee79175c909a9e925c8ed5847d81ae0c
Matthew Zipkin <pinheadmz@gmail.com>
2024-10-16 14:18:45 -0400

    http: Implement HTTPRequest class
    HTTP Request message:
    https://datatracker.ietf.org/doc/html/rfc1945#section-5
    
    Request Line aka Control Line aka first line:
    https://datatracker.ietf.org/doc/html/rfc1945#section-5.1
    
    See message_read_status() in libevent http.c for how
    `MORE_DATA_EXPECTED` is handled there

--- src/httpserver.h
+++ src/httpserver.h
@@ -193,6 +193,14 @@ private:
 };
 
 namespace http_bitcoin {
+using util::LineReader;
+
+// shortest valid request line, used by libevent in evhttp_parse_request_line()
+static const size_t MIN_REQUEST_LINE_LENGTH{strlen("GET / HTTP/1.0")};
+// maximum size of http request (request line + headers)
+// see https://github.com/bitcoin/bitcoin/issues/6425
+static const size_t MAX_HEADERS_SIZE{8192};
+
 enum HTTPStatusCode {
     HTTP_OK = 200,
     HTTP_NO_CONTENT = 204,
@@ -244,6 +252,25 @@ public:
     explicit HTTPResponse(HTTPHeaders* headersIn) : m_headers(headersIn) {}
 
     std::string StringifyHeaders() const;
+};
+
+class HTTPRequest
+{
+public:
+    std::string m_method;
+    std::string m_target;
+    // Default protocol version is used by error responses to unreadable requests
+    int m_version_major{1};
+    int m_version_minor{1};
+    HTTPHeaders m_headers;
+    std::string m_body;
+
+    // Readers return false if they need more data from the
+    // socket to parse properly. They throw errors if
+    // the data is invalid.
+    bool ReadControlData(LineReader& reader);
+    bool ReadHeaders(LineReader& reader);
+    bool ReadBody(LineReader& reader);
 };
 } // namespace http_bitcoin
 
