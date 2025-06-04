f6af0c8cd11a2e4033b248a86880653838cb5a5c
Matthew Zipkin <pinheadmz@pm.me>
2025-06-04 14:14:13 -0400

    SockMan: implement and test AcceptConnection()
    Co-authored-by: Vasil Dimov <vd@FreeBSD.org>

--- src/test/sockman_tests.cpp
+++ src/test/sockman_tests.cpp
@@ -15,15 +15,25 @@ BOOST_AUTO_TEST_CASE(test_sockman)
     SockMan sockman;
 
     // This address won't actually get used because we stubbed CreateSock()
-    const std::optional<CService> addr{Lookup("0.0.0.0", 0, false)};
+    const std::optional<CService> addr_bind{Lookup("0.0.0.0", 0, false)};
     bilingual_str strError;
 
     // Init state
     BOOST_REQUIRE_EQUAL(sockman.m_listen.size(), 0);
     // Bind to mock Listening Socket
-    BOOST_REQUIRE(sockman.BindAndStartListening(addr.value(), strError));
+    BOOST_REQUIRE(sockman.BindAndStartListening(addr_bind.value(), strError));
     // We are bound and listening
     BOOST_REQUIRE_EQUAL(sockman.m_listen.size(), 1);
+
+    // Pick up the phone, there's no one there
+    CService addr_connection;
+    BOOST_REQUIRE(!sockman.AcceptConnection(*sockman.m_listen.front(), addr_connection));
+
+    // Create a mock client and add it to the local CreateSock queue
+    ConnectClient();
+    // Accept the connection
+    BOOST_REQUIRE(sockman.AcceptConnection(*sockman.m_listen.front(), addr_connection));
+    BOOST_CHECK_EQUAL(addr_connection.ToStringAddrPort(), "5.5.5.5:6789");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
