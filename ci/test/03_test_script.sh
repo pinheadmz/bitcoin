32e86600076bbcdf0af712720164e6f19f6d480f
Matthew Zipkin <pinheadmz@pm.me>
2026-09-01 09:59:00 -0400

    narrower tests

--- ci/test/03_test_script.sh
+++ ci/test/03_test_script.sh
@@ -203,14 +203,7 @@ if [ "$RUN_FUNCTIONAL_TESTS" = "true" ]; then
   # parses TEST_RUNNER_EXTRA as an array which allows for multiple arguments such as TEST_RUNNER_EXTRA='--exclude "rpc_bind.py --ipv6"'
   eval "TEST_RUNNER_EXTRA=($TEST_RUNNER_EXTRA)"
   LD_LIBRARY_PATH="${DEPENDS_DIR}/${HOST}/lib" \
-  "${BASE_BUILD_DIR}/test/functional/test_runner.py" \
-    "${MAKEJOBS}" \
-    --tmpdirprefix "${BASE_SCRATCH_DIR}/test_runner/" \
-    --ansi \
-    --combinedlogslen=99999999 \
-    --timeout-factor="1" \
-    "${TEST_RUNNER_EXTRA[@]}" \
-    --failfast
+  "${BASE_BUILD_DIR}/test/functional/interface_http.py" -l DEBUG
 fi
 
 if [ "${RUN_TIDY}" = "true" ]; then
